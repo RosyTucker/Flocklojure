f92cd59aac96ac5de88174994b6d69c22e80c5d7
Rosy Tucker <rosy533@gmail.com>
2014-10-02 20:54:45 +0100

    Delete most things
    Switching to clojurescript

--- src/example/handler.clj
+++
@@ -1,7 +0,0 @@
-(ns example.handler
-  (:require [example.templates :as templates]))
-
-(defn my-handler [request]
-  (templates/render-template "home"
-                             {:message "Hello World"}))
-
--- src/example/templates.clj
+++
@@ -1,9 +0,0 @@
-(ns example.templates
-  (:require [clostache.parser :as clostache]))
-
-(defn read-template-file [template-name]
-  (slurp (clojure.java.io/resource
-           (str "templates/" template-name ".tash.html"))))
-
-(defn render-template [template-name params]
-  (clostache/render (read-template-file template-name) params))
