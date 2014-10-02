864816ed0527e63478aa51f68595b20b7d107975
Rosy Tucker <rosy533@gmail.com>
2014-10-02 22:45:15 +0100

    Initial Drawing Code

--- src/flocking/core.cljs
+++ src/flocking/core.cljs
@@ -1,8 +1,20 @@
 (ns flocking.core
-(:use [flocking.utils :only [set-html! by-id]]))
+  (:require [flocking.utils :as utils]))
 (enable-console-print!)
 
+(defn header []
+      (let [header (utils/by-id :header)]
+           (utils/set-html! header "Hello World")))
+
+(defn canvas []
+      (let [target (utils/by-id :canvas)
+            context (utils/get-context target "2d")]
+           (loop [x 80]
+                 (when (> x 0)
+                 (println x)
+                 (utils/fill-rect context (+ x 100) (+ x 50) x x)
+                 (recur (dec x))))))
+
 (defn ^:export flock []
-      (println "Hello world!")
-      (let [header (by-id :header)]
-           (set-html! header "Hello World")))
+      (header)
+      (canvas))
--- src/flocking/utils.cljs
+++ src/flocking/utils.cljs
@@ -1,23 +1,28 @@
 (ns flocking.utils)
 
 (defn length [nodes]
-        (. nodes -length))
+      (. nodes -length))
 
-  (defn item [nodes n]
-        (.item nodes n))
+(defn item [nodes n]
+      (.item nodes n))
 
-  (defn as-seq [nodes]
-        (for [i (range (length nodes))] (item nodes i)))
+(defn as-seq [nodes]
+      (for [i (range (length nodes))] (item nodes i)))
 
-  (defn by-id [id]
-        (.getElementById js/document (name id)))
+(defn by-id [id]
+      (.getElementById js/document (name id)))
 
-  (defn by-tag [tag]
-        (as-seq
-          (.getElementsByTagName js/document (name tag))))
+(defn by-tag [tag]
+      (as-seq
+        (.getElementsByTagName js/document (name tag))))
 
-  (defn html [dom] (. dom -innerHTML))
+(defn html [dom] (. dom -innerHTML))
 
-  (defn set-html! [dom content]
-        (set! (. dom -innerHTML) content))
+(defn set-html! [dom content]
+      (set! (. dom -innerHTML) content))
 
+(defn get-context [target dim]
+      (.getContext target "2d"))
+
+(defn fill-rect [context left top width height]
+      (.fillRect context left top width height))
