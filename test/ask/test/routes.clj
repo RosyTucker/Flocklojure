ad5ff8297f4ddb73e2ed65e0dbe3f240107bf661
James Thompson <james.thompson@thoughtworks.com>
2014-10-02 11:02:09 +0100

    Jimmy/Rosy - SETUP - Fixed route test

--- test/ask/test/routes.clj
+++ test/ask/test/routes.clj
@@ -1,13 +1,14 @@
-(ns ask.test.handler
+(ns ask.test.routes
   (:require [clojure.test :refer :all]
-            [ask-template.handler :refer :all]
-            [ring.mock.request :as mock]))
+            [ask.routes :refer :all]
+            [ring.mock.request :as mock]
+            [ask.templates :as templates]))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) (templates/render-template "home" {:name "Rosy"})))))
   
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
